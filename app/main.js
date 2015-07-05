add73f5244647564176fc1290b8cb7accf59dfde
Stu <stuart@blueprintinteractive.com>
2015-07-05 12:20:07 -0400

    delete uneccesary images, add route events

--- app/main.js
+++ app/main.js
@@ -1,4 +1,10 @@
 angular.module('myApp',['home','newMeal', 'myEarnings', 'ngAnimate'])
 	.run(function($rootScope) {
 		$rootScope.meals = [];
+		$rootScope.$on('$routeChangeStart', function() {
+			$rootScope.isLoading = true;
+		});
+		$rootScope.$on('$routeChangeSuccess', function() {
+			$rootScope.isLoading = false;
+		});
 	})
